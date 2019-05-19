3a3c14f1bc0403fe364755c47ceeae7f5ee872b6
Francisco Javier Barrera Rodriguez <francisco.barrera@uptc.edu.co>
2019-05-19 16:29:14 -0500

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,146 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import Navbar from './nav/components/navbar';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCashRegister, faShoppingCart, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import HandleError from "./errors/containers/handleError";
+
+library.add(faCashRegister, faShoppingCart, faChevronDown);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <HandleError>
+        <div className="rappicommerce">
+          <Navbar />
+          <div className="rappicommerce-content">
+            <div className="banner">
+              <div className="banner-content color white-color">
+                <h1 className="no-margin no-margin-top text-align text-align-center">Kitchen & Bar</h1>
+                <h4 className="no-margin no-margin-bottom text-align text-align-center">564 Items</h4>
+              </div>
+            </div>
+            <div className="row center-row">
+              <div className="container flex-container">
+                <div className="col-4">
+                  <div className="filters">
+                    <div className="filter-row">
+                      <div>
+                        <label htmlFor="">
+                          Price
+                        </label>
+                        <input type="range" />
+                      </div>
+                    </div>
+                    <div className="filter-row">
+                      <ul className="categories">
+                        <li>
+                          <span>Categoria 1</span>
+                          <ul>
+                            <li>subcategoria 1</li>
+                            <li>subcategoria 2</li>
+                          </ul>
+                        </li>
+                        <li>
+                          <span>Categoria 1</span>
+                          <ul>
+                            <li>subcategoria 1</li>
+                            <li>subcategoria 2</li>
+                          </ul>
+                        </li>
+                      </ul>
+                    </div>
+                    <div className="filter-row">
+                      <div>
+                        <label htmlFor="">Cantidad en stock</label>
+                        <div className="">
+                          <button>-</button>
+                          <label htmlFor="">52</label>
+                          <button>+</button>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="filter-row">
+                      <div>
+                        <label htmlFor="">Disponibilidad</label>
+                        <input type="checkbox"/><span>Disponible</span>
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+                <div className="col-8 grid-products">
+                  <a href="#!" className="card product-card">
+                    <div className="card-header">
+                      <button className="btn btn-nothing">
+                        <FontAwesomeIcon icon="shopping-cart" />
+                      </button>
+                      <span className="card-price">$ price</span>
+                    </div>
+                    <div className="card-body">
+                      <img src="https://www.powerplanetonline.com/cdnassets/smartwatch_x2_02_ad_l.jpg" alt="" />
+                    </div>
+                    <div className="card-footer">
+                      <span>lorem</span>
+                    </div>
+                  </a>
+                  <a href="#!" className="card product-card">
+                    <div className="card-header">
+                      <button className="btn btn-nothing">
+                        <FontAwesomeIcon icon="shopping-cart" />
+                      </button>
+                      <span className="card-price">$ price</span>
+                    </div>
+                    <div className="card-body">
+                      <img src="https://www.powerplanetonline.com/cdnassets/smartwatch_x2_02_ad_l.jpg" alt="" />
+                    </div>
+                    <div className="card-footer">
+                      <span>lorem</span>
+                    </div>
+                  </a>
+                  <a href="#!" className="card product-card">
+                    <div className="card-header">
+                      <button className="btn btn-nothing">
+                        <FontAwesomeIcon icon="shopping-cart" />
+                      </button>
+                      <span className="card-price">$ price</span>
+                    </div>
+                    <div className="card-body">
+                      <img src="https://www.powerplanetonline.com/cdnassets/smartwatch_x2_02_ad_l.jpg" alt="" />
+                    </div>
+                    <div className="card-footer">
+                      <span>lorem</span>
+                    </div>
+                  </a>
+                  <a href="#!" className="card product-card">
+                    <div className="card-header">
+                      <button className="btn btn-nothing">
+                        <FontAwesomeIcon icon="shopping-cart" />
+                      </button>
+                      <span className="card-price">$ price</span>
+                    </div>
+                    <div className="card-body">
+                      <img src="https://www.powerplanetonline.com/cdnassets/smartwatch_x2_02_ad_l.jpg" alt="" />
+                    </div>
+                    <div className="card-footer">
+                      <span>lorem</span>
+                    </div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <footer className="main-footer">
+            <div className="row center-row">
+              <div className="container">
+                <h5>Francisco Javier barrera</h5>
+              </div>
+            </div>
+          </footer>
+        </div>
+      </HandleError>
   );
 }
 
---
+++ src/nav/components/navbar.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import './navbar.scss';
+
+class Navbar extends Component {
+
+    state = {
+        handleError: false
+    }
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <nav className="navbar">
+                <div className="container navbar-container">
+                    <div className="navbar-brand">
+                        <FontAwesomeIcon icon="cash-register" size="2x" />
+                        <h1 className="brand-title">El Baratón</h1>
+                    </div>
+                    <div className="navbar-content">
+                        <ul className="list list-none">
+                            <li>
+                                <button className="btn btn-nothing nav-chart">
+                                    <span>3 Items ($145)</span>
+                                    <FontAwesomeIcon icon="shopping-cart"  className="nav-chart-icon"/>
+                                    <FontAwesomeIcon icon="chevron-down"  className="nav-chart-icon"/>
+                                </button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default Navbar;
